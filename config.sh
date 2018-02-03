e984626f3848ed9ef5dc16a59fa60f9896fecdc1
ZHAOGANG\renjie.zhang <renjie.zhang@china.zhaogang.com>
2018-02-03 22:44:23 +0800

    update

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ import json
 import ConfigParser
 import os
 import sys
+import platform
 
 
 class PropertyDto(object):
@@ -45,22 +46,27 @@ class ConfigCenterAgent(object):
         }
         request = urllib2.Request(url, json.dumps(body))
         request.add_header('Content-Type', 'application/json')
-        request.add_header('snapshot-password', '5e05773e-4359-40e3-9cbf-fc0333846aa2')
+        request.add_header('snapshot-password',
+                           '5e05773e-4359-40e3-9cbf-fc0333846aa2')
         request.get_method = lambda: 'POST'
         response = urllib2.urlopen(request)
 
         snapshot = SnapshotDto()
         data = json.loads(response.read(), 'utf-8')
-        if data['code'] == 0 and data['data'] != None and data['data']['properties'] != None:
+        if data['code'] == 0:
             properties = []
-            for i, element in enumerate(data['data']['properties']):
-                property = PropertyDto()
-                property.__dict__ = element
-                properties.append(property)
-            snapshot.__dict__ = data['data']
+            if data['data'] != None and data['data']['properties'] != None:
+                for i, element in enumerate(data['data']['properties']):
+                    property = PropertyDto()
+                    property.__dict__ = element
+                    properties.append(property)
+                snapshot.__dict__ = data['data']
             snapshot.properties = properties
+        elif data['code'] == -13003:
+            snapshot.properties = []
         else:
-            raise Exception('Failed to get remote properties, message: ' + data['message'] + '.')
+            raise Exception(
+                'Failed to get remote properties, message: ' + data['message'] + '.')
         return snapshot
 
 
@@ -70,10 +76,20 @@ class EnvironmentVariables(object):
     environment = ''
 
     def __init__(self):
-        variables = os.environ
-        self.applicationId = variables.get('systemAlias')
-        self.configCenterBaseUrl = variables.get('CONFIGURL')
-        self.environment = variables.get('CONFIGENV')
+        self.applicationId = os.environ.get('systemAlias')
+        if (self.applicationId == None or self.applicationId == '') and 2 < len(sys.argv):
+            self.applicationId = sys.argv[2]
+        self.configCenterBaseUrl = os.environ.get('CONFIGURL')
+        if (self.configCenterBaseUrl == None or self.configCenterBaseUrl == '') and 3 < len(sys.argv):
+            self.configCenterBaseUrl = sys.argv[3]
+        self.environment = os.environ.get('CONFIGENV')
+        if self.environment == None or self.environment == '':
+            if 4 < len(sys.argv):
+                self.environment = sys.argv[4]
+                if platform.system() == 'Windows' and (self.environment == 'PRD' or self.environment == '40'):
+                    self.environment = 'TEST'
+            else:
+                self.environment = 'TEST'
 
 
 class ConfigFileBuilder(object):
@@ -99,7 +115,6 @@ class ConfigFileBuilder(object):
                 keyValues = types[type]
                 for key in keyValues:
                     self.configParser.set(type, key, keyValues[key])
-        print self.configParser
         file = open(self.configFilePath, 'w')
         self.configParser.write(file)
         file.close()
@@ -122,24 +137,29 @@ class Application(object):
     def run(self):
         environmentVariables = EnvironmentVariables()
         if environmentVariables.applicationId == None or environmentVariables.applicationId == '':
-            raise Exception('Failed to load applicationId from environment variables.')
+            raise Exception(
+                'Failed to load applicationId from environment variables.')
         if environmentVariables.configCenterBaseUrl == None or environmentVariables.configCenterBaseUrl == '':
-            raise Exception('Failed to load configCenterBaseUrl from environment variables.')
+            raise Exception(
+                'Failed to load configCenterBaseUrl from environment variables.')
         if environmentVariables.environment == None or environmentVariables.environment == '':
-            raise Exception('Failed to load from environment environment variables.')
+            raise Exception(
+                'Failed to load from environment environment variables.')
 
         print 'applicationId: %s, environment: %s, configCenterBaseUrl: %s configFilePath: %s' \
             % (environmentVariables.applicationId, environmentVariables.environment, environmentVariables.configCenterBaseUrl, self.configFilePath)
 
         agent = ConfigCenterAgent(environmentVariables.configCenterBaseUrl)
-        snapshot = agent.getRemoteProperties(environmentVariables.applicationId, environmentVariables.environment)
+        snapshot = agent.getRemoteProperties(
+            environmentVariables.applicationId, environmentVariables.environment)
         types = {}
         for i, element in enumerate(snapshot.properties):
             if not types.has_key(element.type):
                 types[element.type] = {}
             types[element.type][element.key] = element.value
 
-        configFileBuilder = ConfigFileBuilder(self.configFilePath + self.configFileName)
+        configFileBuilder = ConfigFileBuilder(
+            self.configFilePath + self.configFileName)
         configFileBuilder.build(types)
 
 
@@ -147,13 +167,17 @@ if __name__ == '__main__':
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    Application().run()
-
-    print 'Success to generate config file.'
+    try:
+        Application().run()
+        print 'Success to generate config file.'
+        sys.exit(0)
+    except Exception as e:
+        print e
+        sys.exit(1)
 " > $pyFileName
 if [ "$configFilePath" == "" ]
 then
-    python2.7 $pyFileName "/data/webconfs"
+    python $pyFileName "/data/webconfs"
 else
-    python2.7 $pyFileName "$configFilePath"
+    python $pyFileName "$configFilePath"
 fi
